b1eabcf0f99b49fc5c6e950c83b4e9918adaded8
namelastamy <silvanaomy37@gmail.com>
2023-05-04 19:52:12 -0400

    task.dialog
    commint naomy

--- lista_de_tarefas/lib/models/task.dart
+++ lista_de_tarefas/lib/models/task.dart
@@ -1,1 +1,27 @@
+class Task {
+  int id;
+  String title;
+  String description;
+  bool isDone;
 
+  Task({
+    this.id,
+    this.title,
+    this.description,
+    this.isDone,
+  });
+
+  factory Task.fromMap(Map<String, dynamic> json) => new Task(
+        id: json["id"],
+        title: json["title"],
+        description: json["description"],
+        isDone: json["isDone"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "title": title,
+        "description": description,
+        "isDone": isDone,
+      };
+}
